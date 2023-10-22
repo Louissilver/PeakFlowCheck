2c79adf695699b12cf37162d0045dabb2f452c5a
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2023-10-22 15:50:14 -0300

    Compartilhamento do gráfico

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -1,51 +1,71 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {LineChart} from 'react-native-chart-kit';
-import {Dimensions} from 'react-native';
+import {Dimensions, Share} from 'react-native';
 import {theme} from '../../styles/globalStyles';
+import ViewShot, {captureRef} from 'react-native-view-shot';
+import {Button} from '../Button';
 
 const ResultsChart = ({line}) => {
-  console.log(line.datasets);
+  const ref = useRef(null);
+
+  const shareImage = async () => {
+    try {
+      const uri = await captureRef(ref, {
+        format: 'png',
+        quality: 0.7,
+      });
+      await Share.share({url: uri});
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <LineChart
-      verticalLabelRotation={30}
-      data={line}
-      height={350}
-      width={Dimensions.get('window').width * 0.95}
-      yAxisLabel={'%'}
-      chartConfig={{
-        propsForBackgroundLines: {
-          stroke: theme.main,
-        },
-        propsForLabels: {
-          fontSize: 15,
-        },
-        propsForDots: {
-          r: '7',
-          strokeWidth: '2',
-        },
-        backgroundGradientFrom: theme.secondary,
-        backgroundGradientTo: theme.secondary,
-        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-      }}
-      getDotColor={(dataPoint, dataPointIndex) => {
-        console.log('dataPoint ---->', dataPoint);
-        console.log('dataPointIndex --->', dataPointIndex);
-        //based on condition we return the color as string
-        if (dataPoint >= 80) {
-          return '#00FF7F';
-        }
-        if (dataPoint < 80 && dataPoint >= 50) {
-          return '#FFD700';
-        }
-        if (dataPoint < 50) {
-          return '#FD6B6B';
-        }
-      }}
-      style={{
-        borderRadius: 10,
-      }}
-      bezier
-    />
+    <>
+      <ViewShot ref={ref}>
+        <LineChart
+          verticalLabelRotation={30}
+          data={line}
+          height={350}
+          width={Dimensions.get('window').width * 0.95}
+          yAxisLabel={'%'}
+          chartConfig={{
+            propsForBackgroundLines: {
+              stroke: theme.main,
+            },
+            propsForLabels: {
+              fontSize: 15,
+            },
+            propsForDots: {
+              r: '7',
+              strokeWidth: '2',
+            },
+            backgroundGradientFrom: theme.secondary,
+            backgroundGradientTo: theme.secondary,
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          }}
+          getDotColor={(dataPoint, dataPointIndex) => {
+            console.log('dataPoint ---->', dataPoint);
+            console.log('dataPointIndex --->', dataPointIndex);
+            //based on condition we return the color as string
+            if (dataPoint >= 80) {
+              return '#00FF7F';
+            }
+            if (dataPoint < 80 && dataPoint >= 50) {
+              return '#FFD700';
+            }
+            if (dataPoint < 50) {
+              return '#FD6B6B';
+            }
+          }}
+          style={{
+            borderRadius: 10,
+          }}
+          bezier
+        />
+      </ViewShot>
+      <Button onPress={shareImage}>Compartilhar gráfico</Button>
+    </>
   );
 };
 
